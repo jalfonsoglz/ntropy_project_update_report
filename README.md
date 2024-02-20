b9b0ec89d4329d5aef79d1c6c37b50f02be5fa43
Alfonso González <32458834+jalfonsoglz@users.noreply.github.com>
2024-02-20 14:03:46 -0600

    v17.0a
    [pending] fix binding id moduel

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-﻿# Módulo de Extensión de Funcionalidades para Ptree
+﻿# Reporte de Actualizaciones de Proyecto
+
+![Impresión del Reporte](static/print_report_pdf.png)
 
 ## Changelog
 
-### 01/03/2023 v16.0.0.0.1
+### 20/02/2024 v17.0
+* Actualización a v17.0
+> [Proximamente] Vinculación Automatica de Reporte  
+
+### 01/03/2023 v16.1
 * Creación del Módulo
 
--- __manifest__.py
+++ __manifest__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Project Update Report",
-    'summary': """Project Update Report""",
-    'description': """Project Update Report""",
+    'summary': """Enable option to print a PDF of projects updates""",
+    'description': """Enable option to print a PDF of projects updates""",
     'author': "Alfonso Gonzalez",
     'website': "https://ntropy.tech/odoo",
     'category': 'Customizations',
-    'version': '16.0.0.0.1',
+    'version': '17.0',
     'license': "LGPL-3",
     'sequence': "-75",
     'depends': ['base',
@@ -28,6 +28,6 @@
     'images': [''],
     'demo': [],
     'installable': True,
-    'application': True,
+    'application': False,
     'auto_install': False,
 }
